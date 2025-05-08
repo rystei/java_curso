9919fc15c749922a4febf4099acd78fdeebdf16d
rystei <gustavo.telles@edu.unifil.br>
2025-05-08 12:59:57 -0300

    Primeiro modulo aula java

---
+++ src/Soma.java
@@ -0,0 +1,8 @@
+public class Soma {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 5;
+        int soma = num1 + num2;
+        System.out.println("A soma é: " + soma);
+    }
+}
