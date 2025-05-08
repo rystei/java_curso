fb19125c75bdf073d30ecbaaf7a7ca367a9beae8
Gustavo <105085153+rystei@users.noreply.github.com>
2025-05-08 13:03:16 -0300

    Add files via upload

---
+++ Estudos.java
@@ -0,0 +1,6 @@
+public class Estudos {
+    public static void main(String[] args) {
+        System.out.println("Segue anotações dos meus estudos");
+        System.out.println("Minhas anotações ------------/");
+    }
+}
---
+++ Perfil.java
@@ -0,0 +1,6 @@
+public class Perfil {
+    public static void main(String[] args) {
+        System.out.println("Olá Gustavo");
+        System.out.println("Tudo bem?");
+    }
+}
