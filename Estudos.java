05fadf4a5464f3c161b0c48538679affadb15aec
Gustavo <105085153+rystei@users.noreply.github.com>
2025-05-12 19:57:41 -0300

    Delete Estudos.java

--- Estudos.java
+++
@@ -1,6 +0,0 @@
-public class Estudos {
-    public static void main(String[] args) {
-        System.out.println("Segue anotações dos meus estudos");
-        System.out.println("Minhas anotações ------------/");
-    }
-}
