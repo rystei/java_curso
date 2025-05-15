e9ecdcabdbae2e9fe8d331852388fd1bda37e6de
rystei <gustavo.telles@edu.unifil.br>
2025-05-15 09:58:24 -0300

    Atualização aula e exercicio1

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,23 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Locale;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+import static java.util.Locale.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        double x = 10.12313;
+        String nome = "Gustavo";
+        int idade = 21;
+        double renda = 1400.0;
+
+        System.out.println(x);
+        System.out.printf("%.3f%n", x);
+        setDefault(US);
+        System.out.printf("%.2f%n", x);
+        System.out.println("Resultado = "+ x + " Metros");
+        System.out.printf("Resultados = %.1f Metros%n", x);
+        System.out.printf("%s tem %d anos e ganha R$ %.2f reais%n", nome, idade, renda);
     }
 }
