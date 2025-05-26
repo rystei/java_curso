6b4051af5d2b08e522120d5848c31f0495d17d0f
rystei <gustavo.telles@edu.unifil.br>
2025-05-26 12:32:27 -0300

    Estrutura sequencial exercicio 5

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,26 @@
-import java.util.Locale;
-
-import static java.util.Locale.*;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
 
-        double x = 10.12313;
-        String nome = "Gustavo";
-        int idade = 21;
-        double renda = 1400.0;
+        int x;
+        String s1, s2, s3;
 
+        x = sc.nextInt();
+        sc.nextLine();
+        s1 = sc.nextLine();
+        s2 = sc.nextLine();
+        s3 = sc.nextLine();
+
+        System.out.println("Dados digitados:");
         System.out.println(x);
-        System.out.printf("%.3f%n", x);
-        setDefault(US);
-        System.out.printf("%.2f%n", x);
-        System.out.println("Resultado = "+ x + " Metros");
-        System.out.printf("Resultados = %.1f Metros%n", x);
-        System.out.printf("%s tem %d anos e ganha R$ %.2f reais%n", nome, idade, renda);
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+
+        sc.close();
     }
 }
