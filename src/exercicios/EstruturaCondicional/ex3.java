c0a530cc44d3379105fcfdb903e5e5f236f127e9
Gustavo Cardoso Telles <gustavo.telles@edu.unifil.br>
2025-06-06 11:43:47 -0300

    Ex estrutura Condicional: numero multiplos

---
+++ src/exercicios/EstruturaCondicional/ex3.java
@@ -0,0 +1,23 @@
+//Leia 2 valores inteiros (A e B). Após, o programa deve mostrar uma mensagem "Sao Multiplos" ou "Nao sao
+//Multiplos", indicando se os valores lidos são múltiplos entre si.
+package exercicios.EstruturaCondicional;
+
+import java.util.Scanner;
+
+public class ex3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro número");
+        int A = scanner.nextInt();
+        System.out.println("Digite o segundo número");
+        int B = scanner.nextInt();
+
+        if (A % B == 0 || B % A == 0){
+            System.out.println("São multiplo");
+        } else {
+            System.out.println("Não são multiplos");
+        }
+        scanner.close();
+    }
+}
