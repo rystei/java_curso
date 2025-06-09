fd832d569a4d19df9f9e76aae01c65a8ed3c41c7
Gustavo Cardoso Telles <gustavo.telles@edu.unifil.br>
2025-06-09 10:53:05 -0300

    Ex estrutura Condicional: X e Y

---
+++ src/exercicios/EstruturaCondicional/ex7.java
@@ -0,0 +1,43 @@
+//Leia 2 valores com uma casa decimal (x e y), que devem representar as coordenadas
+//de um ponto em um plano. A seguir, determine qual o quadrante ao qual pertence o
+//ponto, ou se está sobre um dos eixos cartesianos ou na origem (x = y = 0).
+
+package exercicios.EstruturaCondicional;
+
+import java.util.Scanner;
+
+public class ex7 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        double x = sc.nextDouble();
+        double y = sc.nextDouble();
+
+        if (x == 0.0 && y == 0.0) {
+            System.out.println("Origem");
+        }
+        else if (x == 0.0) {
+            System.out.println("Eixo Y");
+        }
+        else if (y == 0.0) {
+            System.out.println("Eixo X");
+        }
+        else if (x > 0.0 && y > 0.0) {
+            System.out.println("Q1");
+        }
+        else if (x < 0.0 && y > 0.0) {
+            System.out.println("Q2");
+        }
+        else if (x < 0.0 && y < 0.0) {
+            System.out.println("Q3");
+        }
+        else {
+            System.out.println("Q4");
+        }
+
+        sc.close();
+    }
+
+}
+
