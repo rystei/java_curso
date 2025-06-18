487a4abb994b38dc5b11c816fb8adac5b213596a
Gustavo Cardoso Telles <gustavo.telles@edu.unifil.br>
2025-06-18 12:00:29 -0300

    Ex estrutura Condicional: operadores de atribuição cumulativa

---
+++ src/EstruturaCondicional/atribuicaoAcumulativa.java
@@ -0,0 +1,22 @@
+package EstruturaCondicional;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class atribuicaoAcumulativa {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner scanner = new Scanner(System.in);
+
+        int minutos = scanner.nextInt();
+
+        double conta = 50.0;
+        if (minutos > 100) {
+            conta += (minutos - 100) * 2.0;
+        }
+
+        System.out.printf("Valor da conta = R$ %.2f%n", conta);
+        scanner.close();
+    }
+}
