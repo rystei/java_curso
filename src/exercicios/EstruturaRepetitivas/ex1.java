986d81d7189cecc1956c1e200cdabe52ce2f8ff8
rystei <gustavo.telles@edu.unifil.br>
2025-08-04 12:36:20 -0300

    Estrutura Repetitivas: ex 1

---
+++ src/exercicios/EstruturaRepetitivas/ex1.java
@@ -0,0 +1,19 @@
+package exercicios.EstruturaRepetitivas;
+
+import java.util.Scanner;
+
+public class ex1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int senha = 0;
+
+        while (senha != 2002) {
+            System.out.println("Senha Invalida");
+            senha = sc.nextInt();
+        }
+        System.out.println("Acesso Permitido");
+        sc.close();
+    }
+
+}
