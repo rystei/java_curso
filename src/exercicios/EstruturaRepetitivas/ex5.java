d7067971f004b1cc4d5d355b66e8587bea27431c
Your Name <gustavo.telles@edu.unifil.br>
2025-08-05 11:14:13 -0300

    Exercicios for

---
+++ src/exercicios/EstruturaRepetitivas/ex5.java
@@ -0,0 +1,13 @@
+package exercicios.EstruturaRepetitivas;
+
+public class ex5 {
+    public static void main(String[] args) {
+        int y = 0;
+        int x = 0;
+        for (int i=1; i<5; i++){
+            y = i - 1;
+            x = i * 10;
+            System.out.println(i);
+        }
+    }
+}
