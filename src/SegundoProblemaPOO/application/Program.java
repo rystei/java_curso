d1d5b1dc89991879f9253288400c667cb9b39665
Gustavo Telles <gustavo.telles@brconsorcios.com>
2025-08-27 11:48:24 -0300

    Segundo problema usando POO código finalizado

--- src/SegundoProblemaPOO/application/Program.java
+++ src/SegundoProblemaPOO/application/Program.java
@@ -17,9 +17,25 @@ public class Program {
         product.price = sc.nextDouble();
         System.out.print("Quantity in stock: ");
         product.quantity = sc.nextInt();
-        
-        System.out.println(product);
 
+        System.out.println();
+        System.out.println("Product data: " + product);
+
+        System.out.println();
+        System.out.print("Enter the number of products to be added in stock: ");
+        int quantity = sc.nextInt();
+        product.addProducts(quantity);
+
+        System.out.println();
+        System.out.println("Update data: " + product);
+
+        System.out.println();
+        System.out.print("Enter the number of products to be removed in stock: ");
+        quantity = sc.nextInt();
+        product.removeProducts(quantity);
+
+        System.out.println();
+        System.out.println("Update data: " + product);
         sc.close();
     }
 }
