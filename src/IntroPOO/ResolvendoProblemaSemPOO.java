f153e8b3fb840f595add018e155861b0f80ad4da
Gustavo Telles <gustavo.telles@brconsorcios.com>
2025-08-27 10:31:55 -0300

    intro poo triangle calculo

--- src/IntroPOO/ResolvendoProblemaSemPOO.java
+++ src/IntroPOO/ResolvendoProblemaSemPOO.java
@@ -1,29 +1,26 @@
+package IntroPOO;
 import java.util.Scanner;
 
 public class ResolvendoProblemaSemPOO {
     public static void main(String[] args) {
         
         Scanner sc = new Scanner(System.in);
-
-        double xA, xB, xC, yA, yB, yC;
+  
+        Triangle x, y;
+        x = new Triangle();
+        y = new Triangle();        
 
         System.out.println("Enter the measures of triangle X: ");
-        xA = sc.nextDouble();
-        xB = sc.nextDouble();
-        xC = sc.nextDouble();
+        x.a = sc.nextDouble();
+        x.b = sc.nextDouble();
+        x.c = sc.nextDouble();
         System.out.println("Enter the measures of triangle Y: ");
-        yA = sc.nextDouble();
-        yB = sc.nextDouble();
-        yC = sc.nextDouble();
+        y.a = sc.nextDouble();
+        y.b = sc.nextDouble();
+        y.c = sc.nextDouble();
         
-        
-        double p = (xA + xB + xC) / 2;
-
-        double areaX = Math.sqrt(p * (p-xA) * (p-xB) * (p-xC));
-
-        p = (yA + yB + yC) / 2;
-
-        double areaY = Math.sqrt(p * (p-yA) * (p-yB) * (p-yC));
+        double areaX = x.area();
+        double areaY = y.area();
 
         System.out.printf("Triangle X area: %.4f\n", areaX);
         System.out.printf("Triangle Y area: %.4f\n", areaY);
