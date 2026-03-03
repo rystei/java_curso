f3ede62098f4cf59c3fc9a6027a30225d4188c9b
gustavo.telles <gustavo.telles@brconsorcios.com>
2026-03-03 12:10:58 -0300

    Review entrada e saida de dados

--- src/secao3/FirstReview.java
+++ src/secao3/FirstReview.java
@@ -1,7 +1,18 @@
 package secao3;
 
+import java.util.Locale;
+
 public class FirstReview {
-    static void main() {
-        System.out.println("Hello world");
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+
+        double x = 10.35784;
+        System.out.printf("%.2f%n", x);
+        System.out.printf("%.3f%n", x);
+        System.out.println("Resultado = " + x + " metros");
+        System.out.println("Resultado = " + String.format("%.2f", x) + " metros");
     }
 }
+
+
