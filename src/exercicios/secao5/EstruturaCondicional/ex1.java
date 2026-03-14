f3709728eba36b57f32319004663a7aea2dd6b90
rystei <gustavo208cardoso@gmail.com>
2026-03-14 17:30:28 -0300

    review: estrutura condicional exercicio 1

--- src/exercicios/secao5/EstruturaCondicional/ex1.java
+++ src/exercicios/secao5/EstruturaCondicional/ex1.java
@@ -6,5 +6,16 @@ import java.util.Scanner;
 public class ex1 {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+
+        int numero = scanner.nextInt();
+
+        if (numero < 0) {
+            System.out.println("NEGATIVO");
+        } else {
+            System.out.println("NAO NEGATIVO");
+        }
+
+        scanner.close();
     }
 }
