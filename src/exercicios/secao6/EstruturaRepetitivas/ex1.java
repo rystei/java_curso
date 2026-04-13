684238aa8fcbf8b710772118dab7059206a18bec
gustavo.telles <gustavo.telles@brconsorcios.com>
2026-04-13 12:16:05 -0300

    Estruturas repetitivas - for exercicio 1

--- src/exercicios/secao6/EstruturaRepetitivas/ex1.java
+++ src/exercicios/secao6/EstruturaRepetitivas/ex1.java
@@ -5,5 +5,15 @@ import java.util.Scanner;
 public class ex1 {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+
+        int x = scanner.nextInt();
+
+        for (int i=1; i<=x; i++ ) {
+            if ( i % 2 != 0){
+                System.out.println(i);
+            }
+        }
+        scanner.close();
     }
 }
