d85238652a0e810a433914689e79d19fd6a03d04
Your Name <gustavo.telles@edu.unifil.br>
2026-04-21 12:11:55 -0300

    Ex 6

--- src/exercicios/secao6/EstruturaRepetitivas/Ex6.java
+++ src/exercicios/secao6/EstruturaRepetitivas/Ex6.java
@@ -1,4 +1,23 @@
 package exercicios.secao6.EstruturaRepetitivas;
 
+import java.util.Scanner;
+
 public class Ex6 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+
+        for (int i=1; i<=n; i++){
+
+            if (n % i == 0) {
+                System.out.println(i);
+            }
+
+        }
+
+    }
+
 }
