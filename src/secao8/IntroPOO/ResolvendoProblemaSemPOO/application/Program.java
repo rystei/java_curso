ab1f371ebbf1fe253b501e110d82cc549777a095
Your Name <gustavo.telles@edu.unifil.br>
2026-05-18 20:30:16 -0300

    Usando POO para resolver o problema antetior

--- src/secao8/IntroPOO/ResolvendoProblemaSemPOO/application/Program.java
+++ src/secao8/IntroPOO/ResolvendoProblemaSemPOO/application/Program.java
@@ -1,28 +1,35 @@
 package secao8.IntroPOO.ResolvendoProblemaSemPOO.application;
 import java.util.Scanner;
 
+import secao8.IntroPOO.ResolvendoProblemaSemPOO.entities.Triangle;
+
 public class Program {
 
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
+        Triangle x, y;
+
+        x = new Triangle();
+        y = new Triangle();
+
         System.out.println("Enter the measures of triangle X:");
 
-        double area1 = sc.nextDouble();
-        double area2 = sc.nextDouble();
-        double area3 = sc.nextDouble();
+        x.a = sc.nextDouble();
+        x.b = sc.nextDouble();
+        x.c = sc.nextDouble();
 
         System.out.println("Enter the measures of triangle Y:");
 
-        double area4 = sc.nextDouble();
-        double area5 = sc.nextDouble();
-        double area6 = sc.nextDouble();
+        y.a = sc.nextDouble();
+        y.b = sc.nextDouble();
+        y.c = sc.nextDouble();
 
-        double pA = (area1 + area2 + area3) / 2;
-        double areaX = Math.sqrt(pA * (pA - area1) * (pA - area2) * (pA - area3) );
-        double pB = (area4 + area5 + area6) / 2;
-        double areay = Math.sqrt(pB * (pB - area4) * (pB - area5) * (pB - area6) );
+        double pA = (x.a + x.b + x.c) / 2;
+        double areaX = Math.sqrt(pA * (pA - x.a) * (pA - x.b) * (pA - x.c) );
+        double pB = (y.a + y.b + y.c) / 2;
+        double areay = Math.sqrt(pB * (pB - y.a) * (pB - y.b) * (pB - y.c) );
 
 
         System.out.printf("Triangle X area: %.4f%n", areaX);
---
+++ src/secao8/IntroPOO/ResolvendoProblemaSemPOO/entities/Triangle.java
@@ -0,0 +1,9 @@
+package secao8.IntroPOO.ResolvendoProblemaSemPOO.entities;
+
+public class Triangle {
+
+    public double a;
+    public double b;
+    public double c;
+
+}
