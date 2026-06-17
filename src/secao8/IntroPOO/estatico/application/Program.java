a2eb7aa1704bc2aebaaa0791eda7ef7fb8abc09e
gustavo.telles <gustavo.telles@brconsorcios.com>
2026-06-17 12:16:14 -0300

    estatico v2 modo de resolução

--- src/secao8/IntroPOO/estatico/application/Program.java
+++ src/secao8/IntroPOO/estatico/application/Program.java
@@ -7,23 +7,23 @@ import java.util.Scanner;
 
 public class Program {
 
-    public static final double PI = 3.14159;
-
     public static void main(String[] args) {
 
         Locale.setDefault(Locale.US);
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter radius: ");
+        Calculator calc = new Calculator();
+
+        System.out.print("Enter radius: ");
         double radius = scanner.nextDouble();
 
-        double c = Calculator.circumference(radius);
+        double c = calc.circumference(radius);
 
-        double v = Calculator.volume(radius);
+        double v = calc.volume(radius);
 
-        System.out.printf("Circumference: %.2f%n" , c);
-        System.out.printf("Volume: %.2f%n" , v);
-        System.out.printf("PI Value: %.2f%n" , Calculator.PI);
+        System.out.printf("Circumferance: %.2f%n", c);
+        System.out.printf("volume: %.2f%n", v);
+        System.out.printf("PI value: %.2f%n", calc.PI);
 
         scanner.close();
     }
