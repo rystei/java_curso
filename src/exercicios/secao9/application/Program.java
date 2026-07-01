df21fff577f80a7bb4e259afcc498feebdac4009
Your Name <gustavo.telles@edu.unifil.br>
2026-07-01 20:11:29 -0300

    Exercicio de fixação (construtores-this-sobrecarga-encapsulamento) V1

--- src/exercicios/secao9/application/Program.java
+++ src/exercicios/secao9/application/Program.java
@@ -2,10 +2,51 @@ package exercicios.secao9.application;
 
 import exercicios.secao9.entities.Account;
 
+import java.util.Locale;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter account number: ");
+        int number = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.print("Enter account holder: ");
+        String nome = scanner.nextLine();
+
+        System.out.print("Is there na initial deposit (y/n)? ");
+        String initialDeposit = scanner.nextLine();
+
+        Account x = new Account(number, nome);
+
+        if (Objects.equals(initialDeposit, "y")) {
+            System.out.print("Enter initial deposit value: ");
+            double deposito = scanner.nextDouble();
+            x.addDeposito(deposito);
+        }
+
+        System.out.println();
+        System.out.println("Account data:");
+        System.out.println(x);
+
+        System.out.println();
+        System.out.print("Enter a deposit value: ");
+        double depositoAdicionar = scanner.nextDouble();
+        x.addDeposito(depositoAdicionar);
+        System.out.println("Updated account data:");
+        System.out.println(x);
+
+        System.out.println();
+        System.out.print("Enter a withdraw value: ");
+        double depositoRetirar = scanner.nextDouble();
+        x.removeDeposito(depositoRetirar);
+        System.out.println("Updated account data:");
+        System.out.println(x);
 
     }
 }
--- src/exercicios/secao9/entities/Account.java
+++ src/exercicios/secao9/entities/Account.java
@@ -2,4 +2,53 @@ package exercicios.secao9.entities;
 
 public class Account {
 
+    private int numeroConta;
+    private String titularConta;
+    private double deposito;
+
+    public Account(int numeroConta, String titularConta) {
+        this.numeroConta = numeroConta;
+        this.titularConta = titularConta;
+        this.deposito = 0;
+    }
+
+    public int getNumeroConta() {
+        return numeroConta;
+    }
+
+    public String getTitularConta() {
+        return titularConta;
+    }
+
+    public void setTitularConta(String titularConta) {
+        this.titularConta = titularConta;
+    }
+
+    public double getDeposito() {
+        return deposito;
+    }
+
+    public void setDeposito(double deposito) {
+        this.deposito = deposito;
+    }
+
+    public void addDeposito (double x) {
+        deposito += x;
+    }
+
+    private static final double TAXA_SAQUE = 5.0;
+
+    public void removeDeposito (double x) {
+        deposito -= x + TAXA_SAQUE;
+    }
+
+    public String toString() {
+        return "Account " +
+                numeroConta +
+                ", Holder: " +
+                titularConta +
+                ", Balance: $ " +
+                deposito;
+    }
+
 }
