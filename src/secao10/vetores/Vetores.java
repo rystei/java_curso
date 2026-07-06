2db5da89cf2592a844082cc7791cd5aea0758576
Your Name <gustavo.telles@edu.unifil.br>
2026-07-06 20:24:01 -0300

    Criando um vetor

--- src/secao10/vetores/Vetores.java
+++ src/secao10/vetores/Vetores.java
@@ -5,8 +5,31 @@ package secao10.vetores;
 //int[] numeros = new int[5];
 //int[] numeros = {10, 20, 30, 40, 50};
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Vetores {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner scanner = new Scanner(System.in);
+
+        int numero = scanner.nextInt();
+        double[] vetor = new double[numero];
+
+            for (int i = 0; i < numero; i++) {
+            vetor[i] = scanner.nextDouble();
+        }
+
+        double soma = 0;
+        for (int i = 0; i < numero; i++) {
+            soma += vetor[i];
+        }
+
+        double media = soma / numero;
+
+        System.out.printf("A média é:  %.2f%n" , media);
+    }
 
 }
