1631c235a00f93d5b1d42f534f47f0e9480272aa
Rystei <gustavo208cardoso@gmail.com>
2026-07-23 12:18:38 -0300

    Matrizes Teoria

---
+++ src/secao10/matrizes/MatrizTeoria.java
@@ -0,0 +1,23 @@
+package secao10.matrizes;
+
+public class MatrizTeoria {
+
+    /*
+
+    -- Em programação, "matriz" é o nome dado a arranjos bidimensionais (vetor de vetores)
+
+    -- Arranjo (array) é uma estrutura de dados:
+        - Homogênea (dados do mesmo tipo)
+        - Ordenada (elementos acessados por meio de posições)
+        - Alocada de uma vez só, em um bloco contíguo de memória
+
+    -- Vantagens
+        - Acesso imediato aos elementos pela sua posição
+
+    -- Desvantagens:
+        - Tamanho fixo
+        - Dificuldade para se realizar inserções e deleções
+
+     */
+
+}
