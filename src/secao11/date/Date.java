7660213f4d288ade70c662c26aef41d542203a30
Rystei <gustavo208cardoso@gmail.com>
2026-08-12 13:52:57 -0300

    Intro LocalDate

---
+++ src/secao11/date/Date.java
@@ -0,0 +1,13 @@
+package secao11.date;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class Date {
+    public static void main(String[] args) {
+
+        LocalDate date = LocalDate.now();
+        System.out.println(date);
+
+    }
+}
