7620f1bbbd69ce1d5266685f237e1eeacd238f9c
Rystei <gustavo208cardoso@gmail.com>
2026-08-13 20:56:53 -0300

    exemplo java-date

--- src/secao11/date/Date.java
+++ src/secao11/date/Date.java
@@ -1,13 +1,24 @@
 package secao11.date;
 
+import java.time.Instant;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class Date {
     public static void main(String[] args) {
 
+        /*
+        Por padrão a data vem no formato ISO 8601
+         */
+
         LocalDate date = LocalDate.now();
+        LocalDateTime  localDateTime = LocalDateTime.now();
+        Instant instant = Instant.now();
+
         System.out.println(date);
+        System.out.println(localDateTime);
+        System.out.println(instant);
 
     }
 }
